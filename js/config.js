caf0f774342cfe4dfecbb6a62195d8efca879fb7
0x733 <98838267+0x733@users.noreply.github.com>
2024-12-17 21:36:40 +0300

    ☠️

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// js/config.js
+export const stations = [
+    {
+        id: 'trt-fm',
+        name: 'TRT FM',
+        url: 'https://radio-trtfm.live.trt.com.tr/master.m3u8'
+    },
+    {
+        id: 'trt-1',
+        name: 'TRT Radyo 1',
+        url: 'https://radio-trtradyo1.live.trt.com.tr/master.m3u8'
+    },
+    {
+        id: 'trt-3',
+        name: 'TRT Radyo 3',
+        url: 'https://radio-trtradyo3.live.trt.com.tr/master.m3u8'
+    }
+];
