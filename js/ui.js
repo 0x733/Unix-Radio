06a1df04ac56f60800acdff4891e9218479654e6
0x733 <98838267+0x733@users.noreply.github.com>
2024-12-17 21:49:59 +0300

    ☠️

--- js/ui.js
+++ js/ui.js
@@ -4,20 +4,55 @@ export class RadioUI {
         this.container = container;
     }
 
-    render(stations, currentStation) {
+    render(stations, currentStation, isPlaying, volume) {
         this.container.innerHTML = `
             <div class="radio-player">
-                <div class="controls">
-                    <input type="range" class="volume" min="0" max="1" step="0.1" value="1">
+                <div class="player-header">
+                    <h1>Unix Radio</h1>
                 </div>
+                
+                <div class="controls">
+                    <div class="playback-controls">
+                        <button class="btn" id="playBtn">
+                            ${isPlaying ? 'Pause' : 'Play'}
+                        </button>
+                        <button class="btn" id="stopBtn">
+                            Stop
+                        </button>
+                    </div>
+                    
+                    <div class="volume-control">
+                        <span class="volume-icon">🔊</span>
+                        <input type="range" 
+                               class="volume" 
+                               min="0" 
+                               max="1" 
+                               step="0.01" 
+                               value="${volume}">
+                    </div>
+                </div>
+
                 <div class="stations">
                     ${stations.map(station => `
                         <div class="station ${currentStation?.id === station.id ? 'playing' : ''}" 
                              data-station-id="${station.id}">
-                            ${station.name}
+                            <div class="station-info">
+                                <div class="station-name">${station.name}</div>
+                                <div class="station-status">
+                                    ${currentStation?.id === station.id 
+                                        ? (isPlaying ? '▶ Playing' : '❚❚ Paused') 
+                                        : 'Click to play'}
+                                </div>
+                            </div>
                         </div>
                     `).join('')}
                 </div>
+                
+                ${currentStation ? `
+                    <div class="now-playing">
+                        Now Playing: ${currentStation.name}
+                    </div>
+                ` : ''}
             </div>
         `;
     }
@@ -31,8 +66,18 @@ export class RadioUI {
         });
     }
 
+    onPlayClick(callback) {
+        const playBtn = this.container.querySelector('#playBtn');
+        playBtn?.addEventListener('click', callback);
+    }
+
+    onStopClick(callback) {
+        const stopBtn = this.container.querySelector('#stopBtn');
+        stopBtn?.addEventListener('click', callback);
+    }
+
     onVolumeChange(callback) {
         const volume = this.container.querySelector('.volume');
-        volume.addEventListener('input', (e) => callback(e.target.value));
+        volume?.addEventListener('input', (e) => callback(e.target.value));
     }
 }
